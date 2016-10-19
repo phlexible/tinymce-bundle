48c2ba57692a7839ea09531f2d181d033ccc6565
Stephan Wentz <swentz@brainbits.net>
2016-10-19 23:08:55 +0200

    fix docblocks, add LICENSE

--- Controller/DataController.php
+++ Controller/DataController.php
@@ -1,10 +1,14 @@
 <?php
-/**
- * phlexible
+
+/*
+ * This file is part of the phlexible tinymce package.
  *
- * @copyright 2007-2013 brainbits GmbH (http://www.brainbits.net)
- * @license   proprietary
+ * (c) Stephan Wentz <sw@brainbits.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
+
 namespace Phlexible\Bundle\TinymceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
